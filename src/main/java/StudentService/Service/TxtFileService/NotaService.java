53083932bf84063715686d1c876c083df501d97d
imoladezsi <diie2104@scs.ubbcluj.ro>
2019-03-30 23:50:25 +0200

    right folder structure (hopefully)

---
+++ src/main/java/StudentService/Service/TxtFileService/NotaService.java
@@ -0,0 +1,9 @@
+package StudentService.Service.TxtFileService;
+import Domain.*;
+import Repository.TxtFileRepository.NotaFileRepo;
+
+public class NotaService extends AbstractService<Integer,Nota> {
+    public NotaService(NotaFileRepo notaRepo){
+        super(notaRepo);
+    }
+}
