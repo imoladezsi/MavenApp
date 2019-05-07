4febc56c2bfd38a3a645be8e925ee97a469122ea
imoladezsi <diie2104@scs.ubbcluj.ro>
2019-05-07 16:36:41 +0300

    add topdown integration tests

--- src/test/java/Service/Integration/TopDownTesting.java
+++ src/test/java/Service/Integration/TopDownTesting.java
@@ -7,34 +7,17 @@ import Repository.XMLFileRepository.TemaLabXMLRepo;
 import Service.StudentService.XMLFileService.NotaXMLService;
 import Service.StudentService.XMLFileService.StudentXMLService;
 import Service.StudentService.XMLFileService.TemaLabXMLService;
-import UI.ui;
 import Validator.NotaValidator;
 import Validator.StudentValidator;
 import Validator.TemaLabValidator;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 import java.time.LocalDateTime;
 
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertNotNull;
 
-public class BigBangTesting {
-
-
-    public void shouldPassGrade(String[] params){
-        NotaValidator vn=new NotaValidator();
-        NotaXMLRepo ntrepo=new NotaXMLRepo(vn,"NotaXML.xml");
-        NotaXMLService ntsrv=new NotaXMLService(ntrepo);
-
-
-        try {
-            ntsrv.add(params);
-            assertNotNull(ntsrv.findOne(Integer.parseInt(params[0])));
-        }catch(ValidatorException vex){
-            fail();
-        }
-
-    }
+public class TopDownTesting {
     public void shouldPassStudent(String[] params)
     {
         StudentValidator vs=new StudentValidator();
@@ -49,66 +32,33 @@ public class BigBangTesting {
         }
     }
 
-    void shouldPassAssignment(String[] params, String fileName){
-        int id = -1;
-        if (fileName == null)
-            fileName = "TemaLaboratorXML.xml";
-        TemaLabValidator vt=new TemaLabValidator();
-        TemaLabXMLRepo tmrepo=new TemaLabXMLRepo(vt,fileName);
-        TemaLabXMLService tmsrv=new TemaLabXMLService(tmrepo);
-        boolean gotHere = false;
-        try {
-            id = Integer.parseInt(params[0]);
-        }catch(NumberFormatException fe){
-            fail();
-        }
-        tmrepo.delete(id);
-        assertNull(tmrepo.findOne(id));
-        try {
-            tmsrv.add(params);
-            gotHere = true;
-        }catch(ValidatorException e){
-            fail();
-        }
-        assertTrue(gotHere);
-        assertNotNull(tmsrv.findOne(id));
-    }
+    //test case fo addStudent
     @Test
-    public void testAddAssignment_uniqueId() {
-        // contributes to SC
-        // covers in two methods
-        String[] params={"1","text","5","7"};
-        shouldPassAssignment(params, null);
-    }
-    @Test
-    public void shouldPass_addStudent_Id_ConvertibleAlphaNumeric()
+    public void shouldPass_addStudent()
     {
         String[] params={"1aj31","Nume Prenume","933","email@scs.ubbcluj.ro","Nume Prenume"};
         shouldPassStudent(params);
     }
 
+    //integration test for addAssignment (addStudent + addAssignment)
     @Test
-    public void shouldPass_addGrade_uniqueId(){
-        String[] params={"1","text","5","5.5", LocalDateTime.now().toString()};
-        shouldPassGrade(params);
-    }
-
-    @Test
-    public void integration_addStudent_addGrade_addAssignment(){
-
+    public void shouldPass_integration_addStudent_addAssignment()
+    {
         StudentValidator vs=new StudentValidator();
         TemaLabValidator vt=new TemaLabValidator();
-        NotaValidator vn=new NotaValidator();
+       // NotaValidator vn=new NotaValidator();
         StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
         TemaLabXMLRepo tmrepo=new TemaLabXMLRepo(vt,"TemaLaboratorXML.xml");
-        NotaXMLRepo ntrepo=new NotaXMLRepo(vn,"NotaXML.xml");
+       // NotaXMLRepo ntrepo=new NotaXMLRepo(vn,"NotaXML.xml");
         StudentXMLService stsrv=new StudentXMLService(strepo);
         TemaLabXMLService tmsrv=new TemaLabXMLService(tmrepo);
-        NotaXMLService ntsrv=new NotaXMLService(ntrepo);
+       // NotaXMLService ntsrv=new NotaXMLService(ntrepo);
+        NotaXMLService ntsrv = mock(NotaXMLService.class);
 
-        String[] paramsGrade={"1","text","5","5.5", LocalDateTime.now().toString()};
         String[] paramsStudent={"1aj31","Nume Prenume","933","email@scs.ubbcluj.ro","Nume Prenume"};
         String[] paramsAssignment={"1","text","5","7"};
+        String[] paramsGrade={"1","text","5","5.5", LocalDateTime.now().toString()};
+
         boolean here = false;
         try {
             stsrv.add(paramsStudent);
@@ -119,5 +69,40 @@ public class BigBangTesting {
             fail();
         }
         assertTrue(here);
+        assertNotNull(stsrv.findOne("1aj31"));
+        assertNotNull(tmsrv.findOne(1));
+    }
+
+    //integration test for addGrade (addStudent + addAssignment + addGrade)
+    @Test
+    public void shouldPass_integration_addStudent_addAssignment_addGrade()
+    {
+        StudentValidator vs=new StudentValidator();
+        TemaLabValidator vt=new TemaLabValidator();
+        NotaValidator vn=new NotaValidator();
+        StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
+        TemaLabXMLRepo tmrepo=new TemaLabXMLRepo(vt,"TemaLaboratorXML.xml");
+        NotaXMLRepo ntrepo=new NotaXMLRepo(vn,"NotaXML.xml");
+        StudentXMLService stsrv=new StudentXMLService(strepo);
+        TemaLabXMLService tmsrv=new TemaLabXMLService(tmrepo);
+        NotaXMLService ntsrv=new NotaXMLService(ntrepo);
+
+        String[] paramsStudent={"1aj31","Nume Prenume","933","email@scs.ubbcluj.ro","Nume Prenume"};
+        String[] paramsAssignment={"1","text","5","7"};
+        String[] paramsGrade={"1","text","5","5.5", LocalDateTime.now().toString()};
+
+        boolean here = false;
+        try {
+            stsrv.add(paramsStudent);
+            tmsrv.add(paramsAssignment);
+            ntsrv.add(paramsGrade);
+            here = true;
+        }catch(ValidatorException vex){
+            fail();
+        }
+        assertTrue(here);
+        assertNotNull(stsrv.findOne("1aj31"));
+        assertNotNull(tmsrv.findOne(1));
+        assertNotNull(ntsrv.findOne(1));
     }
 }
